2aba21d7bd5e85fb0bcb91f021fd5d78182b22a0
petrchudej <petr.chudej12@seznam.cz>
2022-02-06 17:01:14 +0100

    signUpFor

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.banner-btn').addEventListener('click', () => {
+    document.querySelector('.banner').style.display = 'none';
+    document.querySelector('.form-container').style.cssText = 'opacity: 1; visibility: visible';
+    document.querySelector('.container').style.background = 'linear-gradient(to right, rgba(189,0,255), rgba(0,30,255))'
+});
+document.querySelector('.x-btn').addEventListener('click', () => {
+    document.querySelector('.banner').style.display = 'flex';
+    document.querySelector('.form-container').style.cssText = 'opacity: 0; visibility: hidden'; 
+    document.querySelector('.container').style.cssText = 'linear-gradient(rgba(0,0,0,.8), rgba(0,0,0,.7)), url(images/Bg1.jpg) center no-repeat; background-size: cover';
+})
+
+
+
+// linear-gradient(to right, rgba(189,0,255), rgba(0,30,255)) ;
